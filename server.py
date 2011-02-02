b9a034576322e3261139dbee38687c9fd64f05fa
Z80Fan <z80fan@altervista.org>
2011-02-02 23:33:14 +0100

    Aggiunto il controllo sul SIGINT

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import select
 import threading
 import sys
@@ -18,7 +20,7 @@ class HwIRCserver(threading.Thread):
             self.accepted_clients = accepted_clients
             self.running = True
 
-            print 'Starting acceptor...   ',
+            print 'Starting Acceptor...   ',
             try:
                 self.start()
                 print 'OK'
@@ -30,13 +32,23 @@ class HwIRCserver(threading.Thread):
             return self.connectedNum
 
         def run(self):                                  # Funzione principale del thread, chiamata da .start()
-            while True:                                 # Ciclo di accettazione
-                sock, addr = self.serverSocket.accept() # Attende una connessione in entrata
-                self.client = HwIRCserver.Client(sock)  # Crea un nuovo Client passandogli il socket su cui comunicare
-                self.accepted_clients[sock] = self.client    # Lo aggiunge al dizionario dei Client connessi
-                self.connectedNum += 1                  # Aumenta il numero di client connessi
-        
+            while self.running:                         # Ciclo di accettazione
+                try:
+                    sock, addr = self.serverSocket.accept() # Attende una connessione in entrata
+                    self.client = HwIRCserver.Client(sock)  # Crea un nuovo Client passandogli il socket su cui comunicare
+                    self.accepted_clients[sock] = self.client    # Lo aggiunge al dizionario dei Client connessi
+                    self.connectedNum += 1                  # Aumenta il numero di client connessi
+                except socket.timeout as t:
+                    print "Nessuna connessione in 5 secondi"
+                except socket.error as (errno, msg):        # Alcuni interpreti propagano i segnali (come ctrl+c) anche ai figli del thread principale, quindi questo e' necessario per via della logica del programma
+                    if errno != 4:                          # Se l'errore non e' un SigInt ricevuto
+                        raise                               # propaga l'errore
+                    else:
+                        print "acceptor riceve sigint"      # altrimenti fregatene e mostra un messaggio di debug
+
+
         def stop(self):
+            print "Stopping Acceptor"
             self.running = False
 
     #############################################
@@ -70,14 +82,12 @@ class HwIRCserver(threading.Thread):
             print traceback.print_exc(ex)
             sys.exit(1)
 
-    def __del__(self):
-        stop()
-
     #############################################
     def run(self):
         self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.serverSocket.bind( (self.host, self.port) )
         self.serverSocket.listen(5)              # Indica quanti client possono al massimo rimanere in coda in attesa dell'accept
+        self.serverSocket.settimeout(5)
 
         self.running = True
         self.client_list = {}
@@ -94,17 +104,19 @@ class HwIRCserver(threading.Thread):
                         try:
                             sock.send('Echo:  %s' % data)
                         except Exception as ex:
-                            
+                            print "Errore nel sock.send(), fermo il client."
+                            sock.close()
 
     #############################################
     def stop(self):
+        print "Stopping Server"
         # chiudere tutte le socket.
         sock_list = self.client_list.keys()
         for sock in sock_list:
             sock.close()
         # Esco dal programma
         self.acceptor.stop()    # dico all'acceptor di fermarsi
-        join(acceptor)          # attendo che sia veramente fermo
+        self.acceptor.join()    # attendo che sia veramente fermo
         self.running = False    # imposto per fermarmi anche io
 
 
@@ -113,13 +125,15 @@ if __name__ == '__main__':
     def segnaleUscita(signum, stack):
         print "Interrupt ricevuto (%s)" % signum
         srv1.stop()
-        threading.join(srv1)
+        srv1.join()
         print "Il server e' terminato per via di un interrupt"
         sys.exit(0)
 
     signal.signal(signal.SIGINT, segnaleUscita)
     srv1 = HwIRCserver(socket.gethostname(), 6969)
-    threading.Thread.join(srv1)
+    while srv1.isAlive():
+        srv1.join(4)
+        print "main: attendo"
     print "Il Server e' terminato dal main"
     sys.exit(0)
 
