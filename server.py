b164446d4dcdf704f41928a9838133886e5df8b0
SilverFix <silverfix@gmail.com>
2011-02-06 12:54:24 +0100

    Ho ristrutturato il server utilizzando solo il costrutto di select al posto del binomio acceptor-multithread/select. In tal modo la struttura è _molto_ più lineare, efficiente e leggibile, sono arrivato a questo punto dopo aver strippato parecchio su problemi semplici ma dalla risoluzione assai difficoltosa nel modo in cui era fatto il server prima, uno su tutti il ciclo while True che gestisce attualmente la select nel master branch, non c'è modo (ho provato anche utilizzando semafori, thread event..) per gestirlo come si deve. Anyway ho ramificato in una nuova branch, così ne discutiamo prima: prendilo come un esperimento, dai un occhio a tale versione e fammi sapere cosa ne pensi eventualmente.

--- server.py
+++ server.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import select
-import threading
 import sys
-import traceback
 import socket
 
 # Return codes:
@@ -15,45 +13,9 @@ USERQUIT = 2
 class HwIRCserver():
 
     #############################################
-    # Classe Acceptor: accetta le connessioni in entrata e le smista su vari Client
-    class Acceptor(threading.Thread):
-        def __init__(self, serverSocket, accepted_sockets, accepted_clients, atLeastOneAccepted):
-            threading.Thread.__init__(self)
-            self.connectedNum = 0                       # Numero di client connessi
-            self.serverSocket = serverSocket            # Socket
-            self.accepted_sockets = accepted_sockets
-            self.accepted_clients = accepted_clients
-            self.running = True
-            self.atLeastOneAccepted = atLeastOneAccepted
-            
-        def getNumConnected(self):
-            return self.connectedNum
-
-        def run(self):                                  # Funzione principale del thread, chiamata da .start()
-            while self.running:                         # Ciclo di accettazione
-                try:
-                    sock, addr = self.serverSocket.accept() # Attende una connessione in entrata
-                    client = HwIRCserver.Client(sock)  # Crea un nuovo Client passandogli il socket su cui comunicare
-                    self.accepted_sockets.append(sock) # Aggiunge la socket accettata alla lista delle socket
-                    self.accepted_clients[sock] = client    # Lo aggiunge al dizionario dei Client connessi
-                    self.connectedNum += 1                  # Aumenta il numero di client connessi
-                    self.atLeastOneAccepted.set()
-                except Exception, e:
-                    traceback.print_exc(e)
-                    print 'ACCEPTOR IN. ERROR'
-                    self.running = False
-        
-        def isRunning(self):
-            return self.running
-        
-        def stop(self):
-            print "Stopping Acceptor"
-            self.running = False
-
-    #############################################
     # Classe Client: gestisce le comunicazioni con un singolo client remoto
     class Client():
-        ID = 0                          # ID serve per generare un id per ogni client
+        ID = 0                      # ID serve per generare un id per ogni client
 
         def __init__(self, sock):
             self.__ID = HwIRCserver.Client.ID
@@ -66,60 +28,67 @@ class HwIRCserver():
         def getSock(self):
             return self.__sock
 
+
     #############################################
     def __init__(self, host, port):
-        self.atLeastOneAccepted = threading.Event()
         self.host = host
         self.port = port
+        self.serverSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client_list = {}
-        self.socket_list = []
-        self.running = False
-        self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.acceptor = HwIRCserver.Acceptor(self.serverSocket, self.socket_list, self.client_list, self.atLeastOneAccepted)
-        
+        self.socket_list = [self.serverSock]
+
+    #############################################
+    def getConnectionCount(self):
+        return len(self.client_list)
+    
     #############################################
     def start(self):
-        self.serverSocket.bind( (self.host, self.port) )
-        self.serverSocket.listen(5)              # Indica quanti client possono al massimo rimanere in coda in attesa dell'accept
+        print "Starting Server"
+        self.serverSock.bind( (self.host, self.port) )
+        self.serverSock.listen(5)   # Indica quanti client possono al massimo rimanere in coda in attesa dell'accept
 
-        self.running = True
-        
-        print 'Starting Acceptor...   ',
-        self.acceptor.start()
-        if self.acceptor.isRunning():
-            print 'OK'
-        else:
-            raise Exception('Acceptor: an exception is occured')
-        
-        self.atLeastOneAccepted.wait()         # sock_list non può essere una lista vuota, aspetta almeno un client (a quel punto acceptor setta l'event)
-        while self.running:                    # ciclo di lettura e scrittura
-            ready_to_read, ready_to_write, in_error = select.select(self.socket_list, [], self.socket_list) # il parametro di timeout omesso rende la select bloccante, sarebbe cpukill
+        while True:                 # ciclo di lettura e scrittura
+            ready_to_read, ready_to_write, in_error = select.select(self.socket_list, [], self.socket_list) # Il parametro di timeout omesso rende la select bloccante, sarebbe cpukill
             for sock in ready_to_read:
-                try:
-                    data = sock.recv(1024)
-                    print "[%s] %s" % (self.client_list[sock].getID(), data),
-                    sock.send('Echo:  %s' % data)
-                except Exception as ex:
-                    print "Errore nel sock.send(), fermo il client."
-                    sock.close()
+                if sock == self.serverSock:
+                    clientSock = self.serverSock.accept()[0]
+                    self.client_list[clientSock] = HwIRCserver.Client(clientSock)
+                    self.socket_list.append(clientSock)
+                    print '------ connection count: %s ------' % self.getConnectionCount()
+                else:
+                    try:
+                        data = sock.recv(256)
+                        if not data: raise
+                        print "[%s] %s" % (self.client_list[sock].getID(), data),
+                        sock.send('Echo:  %s' % data)
+                    except:
+                        print "Client disconnected"
+                        sock.close()
+                        self.socket_list.remove(sock)
+                        del self.client_list[sock]
+                        print '------ connection count: %s ------' % self.getConnectionCount()
 
     #############################################
     def stop(self):
         print "Stopping Server"
-        # chiudere tutte le socket.
+        # chiudere tutte le socket
         for sock in self.socket_list:
             sock.close()
-        # Esco dal programma
-        if self.acceptor.isAlive() and self.acceptor.isRunning():
-            self.acceptor.stop()    # dico all'acceptor di fermarsi
-            self.acceptor.join()    # attendo che sia veramente fermo
-        self.running = False    # imposto per fermarmi anche io
-
-
-if __name__ == '__main__':
-    # srv = HwIRCserver(socket.gethostname(), 6969) # Map outside LAN
-    srv = HwIRCserver('', 6969)
     
+    #############################################
+    def restart(self):
+        print "Restarting Server"
+        self.stop()
+        self.__init__(self.host, self.port)
+        self.start()
+    
+   
+if __name__ == '__main__':
+    if '--listen-outside' in sys.argv:
+        srv = HwIRCserver('', 6969) # Map either localhost and LAN 
+    else:
+        srv = HwIRCserver('127.0.0.1', 6969) # Map in LAN
+        
     try:
         print 'Server started...   '
         srv.start()
@@ -127,10 +96,11 @@ if __name__ == '__main__':
         srv.stop()
         print "Server aborted due to CTRL-C signal"
         sys.exit(USERQUIT)
-    except Exception as exc:
-        print "Server aborted due to exception: ", traceback.print_exc(exc)
-        srv.stop()
-        sys.exit(ABEND)
-    else:
+    except SystemExit: # Viene sollevata alla chiamata di sys.exit()
         print "Server ended"
         sys.exit(GRACEFULLY)
+    except Exception as ex:
+        print "Server aborted due to exception: ", ex
+        srv.stop()
+        sys.exit(ABEND)
+
