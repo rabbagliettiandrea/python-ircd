ee775e2fb36e787d192b6450e5d70e44848edc05
Z80Fan <z80fan@altervista.org>
2011-02-06 20:28:38 +0100

    Aggiunti commenti

--- server.py
+++ server.py
@@ -40,7 +40,7 @@ class HwIRCserver():
     #############################################
     def getConnectionCount(self):
         return len(self.client_list)
-    
+
     #############################################
     def start(self):
         print "Starting Server"
@@ -48,23 +48,26 @@ class HwIRCserver():
         self.serverSock.listen(5)   # Indica quanti client possono al massimo rimanere in coda in attesa dell'accept
 
         while True:                 # ciclo di lettura e scrittura
+        	# Con la select otteniamo la lista dei client che hanno dei dati da inviare (ready_to_read), che sono pronti a ricevere (ready_to_write), e quelli in errore (in_error)
             ready_to_read, ready_to_write, in_error = select.select(self.socket_list, [], self.socket_list) # Il parametro di timeout omesso rende la select bloccante, sarebbe cpukill
+            #Iteriamo attraverso tutti i client che hanno dati da inviare (quindi il server li deve ricevere)
             for sock in ready_to_read:
-                if sock == self.serverSock:
-                    clientSock = self.serverSock.accept()[0]
-                    self.client_list[clientSock] = HwIRCserver.Client(clientSock)
+                if sock == self.serverSock:			# Se uno di questi socket è quello del server, significa che un nuovo client si è connesso
+                    clientSock = self.serverSock.accept()[0]	# Accettiamo il nuovo client
+                    print " - new client accepted"
+                    self.client_list[clientSock] = HwIRCserver.Client(clientSock)	# Lo aggiungiamo alle nostre liste
                     self.socket_list.append(clientSock)
                     print '------ connection count: %s ------' % self.getConnectionCount()
-                else:
+                else:								# Altrimenti, uno dei client ha dati da inviare
                     try:
-                        data = sock.recv(256)
-                        if not data: raise
+                        data = sock.recv(256)		# riceviamo questi dati
+                        if not data: raise			# Se non abbiamo ricevuto dati, alziamo un'eccezione
                         print "[%s] %s" % (self.client_list[sock].getID(), data),
                         sock.send('Echo:  %s' % data)
-                    except:
+                    except:							# Se non abbiamo ricevuto dati, il client si è sconnesso
                         print "Client disconnected"
                         sock.close()
-                        self.socket_list.remove(sock)
+                        self.socket_list.remove(sock)	# Lo rimuoviamo dalle lista
                         del self.client_list[sock]
                         print '------ connection count: %s ------' % self.getConnectionCount()
 
@@ -88,11 +91,11 @@ if __name__ == '__main__':
         srv = HwIRCserver('', 6969) # Map either localhost and LAN 
     else:
         srv = HwIRCserver('127.0.0.1', 6969) # Map in LAN
-        
+
     try:
         print 'Server started...   '
         srv.start()
-    except KeyboardInterrupt:
+    except KeyboardInterrupt:	# Sollevata quando si preme CTRL+C
         srv.stop()
         print "Server aborted due to CTRL-C signal"
         sys.exit(USERQUIT)
