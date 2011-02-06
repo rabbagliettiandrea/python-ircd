2a17150acf794743462d78abf2babf390e4187ed
SilverFix <silverfix@gmail.com>
2011-02-06 12:22:58 +0100

    primo nomultiserver

--- server.py
+++ server.py
@@ -5,29 +5,27 @@ import threading
 import sys
 import traceback
 import socket
-import signal
+
+# Return codes:
+ABEND = 1
+GRACEFULLY = 0
+USERQUIT = 2
 
 
-class HwIRCserver(threading.Thread):
+class HwIRCserver():
 
     #############################################
     # Classe Acceptor: accetta le connessioni in entrata e le smista su vari Client
     class Acceptor(threading.Thread):
-        def __init__(self, serverSocket, accepted_clients ):
+        def __init__(self, serverSocket, accepted_sockets, accepted_clients, atLeastOneAccepted):
             threading.Thread.__init__(self)
             self.connectedNum = 0                       # Numero di client connessi
             self.serverSocket = serverSocket            # Socket
+            self.accepted_sockets = accepted_sockets
             self.accepted_clients = accepted_clients
             self.running = True
-
-            print 'Starting Acceptor...   ',
-            try:
-                self.start()
-                print 'OK'
-            except Exception as ex:
-                print traceback.print_exc(ex)
-                sys.exit(1)
-
+            self.atLeastOneAccepted = atLeastOneAccepted
+            
         def getNumConnected(self):
             return self.connectedNum
 
@@ -35,18 +33,19 @@ class HwIRCserver(threading.Thread):
             while self.running:                         # Ciclo di accettazione
                 try:
                     sock, addr = self.serverSocket.accept() # Attende una connessione in entrata
-                    self.client = HwIRCserver.Client(sock)  # Crea un nuovo Client passandogli il socket su cui comunicare
-                    self.accepted_clients[sock] = self.client    # Lo aggiunge al dizionario dei Client connessi
+                    client = HwIRCserver.Client(sock)  # Crea un nuovo Client passandogli il socket su cui comunicare
+                    self.accepted_sockets.append(sock) # Aggiunge la socket accettata alla lista delle socket
+                    self.accepted_clients[sock] = client    # Lo aggiunge al dizionario dei Client connessi
                     self.connectedNum += 1                  # Aumenta il numero di client connessi
-                except socket.timeout as t:
-                    print "Nessuna connessione in 5 secondi"
-                except socket.error as (errno, msg):        # Alcuni interpreti propagano i segnali (come ctrl+c) anche ai figli del thread principale, quindi questo e' necessario per via della logica del programma
-                    if errno != 4:                          # Se l'errore non e' un SigInt ricevuto
-                        raise                               # propaga l'errore
-                    else:
-                        print "acceptor riceve sigint"      # altrimenti fregatene e mostra un messaggio di debug
-
-
+                    self.atLeastOneAccepted.set()
+                except Exception, e:
+                    traceback.print_exc(e)
+                    print 'ACCEPTOR IN. ERROR'
+                    self.running = False
+        
+        def isRunning(self):
+            return self.running
+        
         def stop(self):
             print "Stopping Acceptor"
             self.running = False
@@ -69,76 +68,69 @@ class HwIRCserver(threading.Thread):
 
     #############################################
     def __init__(self, host, port):
-        threading.Thread.__init__(self)
-
+        self.atLeastOneAccepted = threading.Event()
         self.host = host
         self.port = port
-
-        print 'Starting Server...   ',
-        try:
-            self.start()
-            print 'OK'
-        except Exception as ex:
-            print traceback.print_exc(ex)
-            sys.exit(1)
-
-    #############################################
-    def run(self):
+        self.client_list = {}
+        self.socket_list = []
+        self.running = False
         self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.acceptor = HwIRCserver.Acceptor(self.serverSocket, self.socket_list, self.client_list, self.atLeastOneAccepted)
+        
+    #############################################
+    def start(self):
         self.serverSocket.bind( (self.host, self.port) )
         self.serverSocket.listen(5)              # Indica quanti client possono al massimo rimanere in coda in attesa dell'accept
-        self.serverSocket.settimeout(5)
 
         self.running = True
-        self.client_list = {}
-        self.acceptor = HwIRCserver.Acceptor(self.serverSocket, self.client_list)
-
-        while self.running:                     # ciclo di lettura e scrittura
-            sock_list = self.client_list.keys()
-            if sock_list:
-                ready_to_read, ready_to_write, in_error = select.select(sock_list, sock_list, sock_list)
-                for sock in ready_to_read:
+        
+        print 'Starting Acceptor...   ',
+        self.acceptor.start()
+        if self.acceptor.isRunning():
+            print 'OK'
+        else:
+            raise Exception('Acceptor: an exception is occured')
+        
+        self.atLeastOneAccepted.wait()         # sock_list non può essere una lista vuota, aspetta almeno un client (a quel punto acceptor setta l'event)
+        while self.running:                    # ciclo di lettura e scrittura
+            ready_to_read, ready_to_write, in_error = select.select(self.socket_list, [], self.socket_list) # il parametro di timeout omesso rende la select bloccante, sarebbe cpukill
+            for sock in ready_to_read:
+                try:
                     data = sock.recv(1024)
                     print "[%s] %s" % (self.client_list[sock].getID(), data),
-                    if sock in ready_to_write:
-                        try:
-                            sock.send('Echo:  %s' % data)
-                        except Exception as ex:
-                            print "Errore nel sock.send(), fermo il client."
-                            sock.close()
+                    sock.send('Echo:  %s' % data)
+                except Exception as ex:
+                    print "Errore nel sock.send(), fermo il client."
+                    sock.close()
 
     #############################################
     def stop(self):
         print "Stopping Server"
         # chiudere tutte le socket.
-        sock_list = self.client_list.keys()
-        for sock in sock_list:
+        for sock in self.socket_list:
             sock.close()
         # Esco dal programma
-        self.acceptor.stop()    # dico all'acceptor di fermarsi
-        self.acceptor.join()    # attendo che sia veramente fermo
+        if self.acceptor.isAlive() and self.acceptor.isRunning():
+            self.acceptor.stop()    # dico all'acceptor di fermarsi
+            self.acceptor.join()    # attendo che sia veramente fermo
         self.running = False    # imposto per fermarmi anche io
 
 
 if __name__ == '__main__':
-
-    def segnaleUscita(signum, stack):
-        print "Interrupt ricevuto (%s)" % signum
-        srv1.stop()
-        srv1.join()
-        print "Il server e' terminato per via di un interrupt"
-        sys.exit(0)
-
-    signal.signal(signal.SIGINT, segnaleUscita)
-    srv1 = HwIRCserver(socket.gethostname(), 6969)
-    while srv1.isAlive():
-        srv1.join(4)
-        print "main: attendo"
-    print "Il Server e' terminato dal main"
-    sys.exit(0)
-
-
-
-
-
-
+    # srv = HwIRCserver(socket.gethostname(), 6969) # Map outside LAN
+    srv = HwIRCserver('', 6969)
+    
+    try:
+        print 'Server started...   '
+        srv.start()
+    except KeyboardInterrupt:
+        srv.stop()
+        print "Server aborted due to CTRL-C signal"
+        sys.exit(USERQUIT)
+    except Exception as exc:
+        print "Server aborted due to exception: ", traceback.print_exc(exc)
+        srv.stop()
+        sys.exit(ABEND)
+    else:
+        print "Server ended"
+        sys.exit(GRACEFULLY)
