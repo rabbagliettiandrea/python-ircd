0829285d16f75f0533bd4eaf6bd28cdc694bad96
Z80Fan <z80fan@altervista.org>
2011-02-10 14:33:41 +0100

    Minor editing

--- server.py
+++ server.py
@@ -46,7 +46,7 @@ class Server():
 
     #############################################
     def start(self):
-        print "Starting Server"
+        print "-L- Starting Server"
         self.serverSock.bind( (self.host, self.port) )
         self.serverSock.listen(5)   # Indica quanti client possono al massimo rimanere in coda in attesa dell'accept
 
@@ -58,10 +58,10 @@ class Server():
             for sock in ready_to_read:
                 if sock == self.serverSock:			            # Se uno di questi socket è quello del server, significa che un nuovo client si è connesso
                     clientSock = self.serverSock.accept()[0]    # Accettiamo il nuovo client
-                    print "- new client accepted"
+                    print "-L- new client accepted"
                     self.client_list[clientSock] = Server.Client(clientSock)	# Lo aggiungiamo alle nostre liste
                     self.socket_list.append(clientSock)
-                    print '------ connection count: %s ------' % self.getConnectionCount()
+                    print '-L- - connection count: %s' % self.getConnectionCount()
                 else:								# Altrimenti, uno dei client ha dati da inviare
                     try:
                         data = sock.recv(256)		# Riceviamo questi dati
@@ -71,27 +71,27 @@ class Server():
                         if not client.logged:       # Se il client non si è ancora loggato nella rete
                             # Elaboriamo [pass/]nick/user
                             dataSplit = data.lower().strip().split() # split() suddivide la stringa in una lista d'istruzioni, strip() rimuove il newline finale
-                            if dataSplit[0] == "pass": 
+                            if dataSplit[0] == "pass":
                                 if not client.password and not client.nick:                          
                                     if irc_regex.getConnectionRegex()['pass'].match(dataSplit[1]):
                                         client.password = dataSplit[1]
                                         sock.send('OK\n')
                                     else:
-                                        sock.send('Il formato della password è illegale\n')
+                                        sock.send('-E- Il formato della password è illegale\n')
                                 else:
-                                    sock.send("-E- Password già inviata o inviata dopo un nick ---\n")   # ovviamente da mettere secondo lo standard
+                                    sock.send("-E- Password già inviata o inviata dopo un nick\n")   # ovviamente da mettere secondo lo standard
                             elif dataSplit[0] == "nick":
                                 if not client.nick:
                                     if irc_regex.getConnectionRegex()['nick'].match(dataSplit[1]):
                                         client.nick = dataSplit[1]
                                         sock.send('OK\n')
                                     else:
-                                        sock.send('Il formato del nick è illegale\n')
+                                        sock.send('-E- Il formato del nick è illegale\n')
                                 else:
-                                    sock.send(" -E- Nick già inviato ---")                              # ovviamente da mettere secondo lo standard
+                                    sock.send("-E- Nick già inviato ---\n")                              # ovviamente da mettere secondo lo standard
                             elif dataSplit[0] == "user":
                                 if not client.user and client.nick:
-                                    if len(dataSplit)>4 and irc_regex.getConnectionRegex()['user'].match(dataSplit[1]) and dataSplit[2] == '0' and dataSplit[3] == '*':
+                                    if len(dataSplit)>4 and irc_regex.getConnectionRegex()['user'].match(dataSplit[1]) and (dataSplit[2] in ['0', '4', '8', '12']):
                                         # visto che realname può contenere spazi tramite la list comprehension otteniamo la lista contenente tutti i segmenti del realname
                                         realname = ' '.join([ segm for segm in dataSplit[4:] ]) # successivamente joiniamo questi segmenti insieme con ' '
                                         if irc_regex.getConnectionRegex()['realname'].match(realname):
@@ -99,57 +99,61 @@ class Server():
                                             client.realname = realname
                                             client.logged = True
                                             client.flags.append(dataSplit[2])
-                                            sock.send('OK\n')
+                                            sock.send('OK, logged in\n')
+                                        else:
+                                            sock.send('-E- Il formato del realname è illegale\n')
                                     else:
-                                        sock.send('Il formato dello user è illegale\n')
+                                        sock.send('-E- Il formato dello user è illegale\n')
                                 else:
-                                    sock.send("-E- User già inviato o non è stato inviato prima nick ---\n")      # ovviamente da mettere secondo lo standard
+                                    sock.send("-E- User già inviato o non è stato inviato prima nick\n")      # ovviamente da mettere secondo lo standard
                             else:
-                                sock.send("-E- Comando non valido ---\n")       # ovviamente da mettere secondo lo standard
+                                sock.send("-E- Comando non valido\n")       # ovviamente da mettere secondo lo standard
                         else:
                             # elaboriamo i comandi normali
-                            print "[%s] %s" % (self.client_list[sock].ID, data),
+                            print "-M- [%s] %s" % (self.client_list[sock].ID, data),
                             sock.send('Echo:  %s' % data)
                     except Exception as e:							# Se non abbiamo ricevuto dati, il client si è sconnesso
-                        print "Client disconnected:", e
+                        print "-L- Client disconnected:", e
                         sock.close()
                         self.socket_list.remove(sock)	# Lo rimuoviamo dalle lista
                         del self.client_list[sock]
-                        print '------ connection count: %s ------' % self.getConnectionCount()
+                        print '-L- - connection count: %s' % self.getConnectionCount()
 
     #############################################
     def stop(self):
-        print "Stopping Server"
+        print "-L- Stopping Server"
         # chiudere tutte le socket
         for sock in self.socket_list:
             sock.close()
-    
+
     #############################################
     def restart(self):
-        print "Restarting Server"
+        print "-L- Restarting Server"
         self.stop()
         self.__init__(self.host, self.port)
         self.start()
-    
-   
+
+
 if __name__ == '__main__':
+	print "--- Hardware Upgrade IRC Server project ---"
+
     if '--listen-outside' in sys.argv:
         srv = Server('', 6969) # Map either localhost and LAN 
     else:
         srv = Server('127.0.0.1', 6969) # Map in LAN
 
     try:
-        print 'Server started...   '
+        print '-L- Server started...   '
         srv.start()
     except KeyboardInterrupt:	# Sollevata quando si preme CTRL+C
         srv.stop()
-        print "Server aborted due to CTRL-C signal"
+        print "-L-Server aborted due to CTRL-C signal"
         sys.exit(USERQUIT)
     except SystemExit: # Viene sollevata alla chiamata di sys.exit()
-        print "Server ended"
+        print "-L- Server ended"
         sys.exit(GRACEFULLY)
     except Exception as ex:
-        print "Server aborted due to exception: ", ex
+        print "-L- Server aborted due to exception: ", ex
         srv.stop()
         sys.exit(ABEND)
 
