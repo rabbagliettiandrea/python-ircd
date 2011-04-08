f42684c60946e633b137ea4679df08589a97df24
SilverFix <silverfix@gmail.com>
2011-04-08 02:17:35 +0200

    pingtimeout funzionante

--- py_ircd/connection.py
+++ py_ircd/connection.py
@@ -7,17 +7,38 @@ from twisted.internet import reactor
 
 from py_ircd.utils import *
 
+
+class PingTimeoutHandler():
+    
+    def __init__(self, connection):
+        self.connection = connection
+        self.idle_time = 0
+        self.loop = LoopingCall(self._handle)
+        self.disconnect_schedule = None
+        reactor.callLater(1, self.loop.start, 1)
+        
+    def _handle(self):
+        self.idle_time += 1
+        if self.idle_time == 5:
+            self.connection.send(":%s PING" % self.connection.get_ident())
+            self.disconnect_schedule = reactor.callLater(5, self.connection.quit, 'Ping timeout: 240 seconds')
+    
+    def refresh(self):
+        self.idle_time = 0
+        if self.disconnect_schedule and self.disconnect_schedule.active():
+            self.disconnect_schedule.cancel()
+    
+    def stop(self):
+        self.loop.stop()
+
+
 class Connection(LineOnlyReceiver):
     
     ID = 0
     delimiter = '\n' # overrida il delimitatore di LineOnlyReceiver, '\r\n'  
     
     def connectionMade(self):
-        self.idle_time = 0
-        self.check_alive_loop = LoopingCall(self.handle_idle_time)
-        self.timeout_delayed = None
-        reactor.callLater(1, self.check_alive_loop.start, 1)
-        
+        self.timeout_handler = PingTimeoutHandler(self)
         self.factory.connections_count += 1
         self.ID = Connection.ID
         Connection.ID += 1
@@ -27,7 +48,7 @@ class Connection(LineOnlyReceiver):
         print_log('connection count: %s' % self.factory.connections_count)
 
     def connectionLost(self, reason):
-        self.check_alive_loop.stop()
+        self.timeout_handler.stop()
         self.factory.connections_count -= 1
         if not reason.check(ConnectionDone):
             self.quit()
@@ -41,12 +62,5 @@ class Connection(LineOnlyReceiver):
 
     def dataReceived(self, data):
         LineOnlyReceiver.dataReceived(self, data)
-        if self.timeout_delayed:
-            self.timeout_delayed.cancel()
-        self.idle_time = 0
-    
-    def handle_idle_time(self):
-        self.idle_time += 1
-        if self.idle_time == 5:
-            self.send(":%s PING" % self.get_ident())
-            self.timeout_delayed = reactor.callLater(5, self.quit, 'Ping timeout: 240 seconds')
+        self.timeout_handler.refresh()
+
